61abaeec2bd6ab5bde25e177fa09152ae584fb32
Felipe B Druzian <felipebdruzian@gmail.com>
2022-01-12 15:15:09 -0300

    prototype v0.4

--- prototype/proj.ino
+++ prototype/proj.ino
@@ -8,16 +8,17 @@ RTC_DS3231 rtc;
 #define Botao1 11
 
 #define Vazao 2
-double fluxo = 0;
-double volume = 0;
-double volumeTotal = 0;
+const float  calibracao = 6.41; //calibrado com regra de 3
+float fluxo = 0;
+float volume = 0;
+float volumeTotal = 0;
 volatile int contador;
 
 unsigned long tempoIni = 0;
-unsigned long tempoAtu;
+//unsigned long tempoAtu;
 const unsigned long periodo = 500;
 
-char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+//char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
 bool estadoRele = 1; //  1 = desligado / 0 = ligado  (negocio sem nexo da p***)
 bool estadoLed = 0;
 
@@ -27,6 +28,9 @@ void setup()
     pinMode(Rele, OUTPUT); //define o pino do Rele e do Led como OUTPUT (Saida de dados)
     digitalWrite(Rele, HIGH); //inicializa com o rele desligado
     pinMode(Botao1, INPUT_PULLUP); //define o pino do Botao como INPUT (Entrada de dados)
+
+    pinMode(Vazao, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(2), contadorPulso, RISING); //funcao q define o pino de interrupcao
 
     Serial.begin(9600); //inicializa comunicação serial
     lcd.begin(16, 2);
@@ -41,11 +45,6 @@ void setup()
     {
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //Configura o relogio pra hora que o sketch foi compilado
     }
-    
-
-    pinMode(Vazao, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(2), contadorPulso, RISING);
-
 }
 
 void loop()
@@ -79,7 +78,8 @@ void loop()
         estadoLed = !estadoLed;
 
         lcd.clear();
-        lcd.setCursor(0,14); //muda o apenas estado do rele no display
+        lcd.setCursor(0,0); //muda o apenas estado do rele no display
+        lcd.print("Estado Rele: ");
         if (estadoRele == 0)
         {
             lcd.print("ON"); //0 == LOW == Rele Ligado (pq sim, rele maluco)
@@ -90,43 +90,35 @@ void loop()
         digitalWrite(LED_BUILTIN, estadoLed);
     } 
 
-        //fluxo = contador * 2.25;
-        //fluxo *= 60;
-        //fluxo /= 1000;
-
-        //volume = fluxo / 60;
-        //volumeTotal += volume;
-
-
-    while (estadoRele == 0)
+    while (estadoRele == 0) //enquanto o rele estiver ligado
     {
-        //tempoIni = millis();
-        if (millis() - tempoIni >= periodo) 
+        if (millis() - tempoIni >= periodo) //vai repetir esse if a cada periodo(ms) de tempo
         {
-            tempoIni += periodo;
-            noInterrupts();
+            noInterrupts(); //desabilita a Interrupcao para fazer os calculos
 
-
-            fluxo = contador;
-            contador = 0;
-            interrupts();
+            fluxo = ((periodo / (millis() - tempoIni)) * contador) / calibracao;
+            volume = fluxo / 60;
+            volumeTotal += volume;
             
-            lcd.setCursor(0,0);
-            lcd.clear();
+            lcd.setCursor(0,1);
             lcd.print("Volume: ");
-            lcd.print(fluxo);
+            lcd.print(volumeTotal);
             lcd.print(" L");
-            Serial.println("morri xc");
             
+            contador = 0;
+            tempoIni = millis();
+            interrupts(); //habilita a Interrupcao para medir os pulsos
         }
+
         if (digitalRead(Botao1) != 1) {
             delay(400);
             estadoRele = !estadoRele;
             digitalWrite(Rele, estadoRele);
+            lcd.clear();
         }
     }
 }
 
-void contadorPulso() { //funcao ta ok
+void contadorPulso() { //funcao que conta os pulsos do sensor de vazao
     contador++;
 }
