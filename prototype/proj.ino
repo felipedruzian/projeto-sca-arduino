a81cc48a7cb8067526b3f058f7f726ee03f23990
Felipe B Druzian <felipebdruzian@gmail.com>
2022-01-12 21:17:02 -0300

    prototype v0.46

--- prototype/proj.ino
+++ prototype/proj.ino
@@ -6,8 +6,8 @@ RTC_DS3231 rtc;
 
 #define Botao1 10
 #define Botao2 11
-#define Botao3 13
-#define Rele 12 //o rele no 12 pq no 13 ele tava bugando (motivo: sla)
+#define Botao3 12
+#define Rele 3 //o rele no 2 pq no 13 ele tava bugando (motivo: sla)
 
 #define Vazao 2
 const float  calibracao = 6.41; //calibrado com regra de 3
@@ -24,14 +24,12 @@ const unsigned long periodo = 500;
 
 //char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
 bool estadoRele = 1; //  1 = desligado / 0 = ligado  (negocio sem nexo da p***)
-bool estadoLed = 0;
 
 int opc = 0;
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT); 
-    pinMode(Rele, OUTPUT); //define o pino do Rele e do Led como OUTPUT (Saida de dados)
+    pinMode(Rele, OUTPUT); //define o pino do Rele como OUTPUT (Saida de dados)
     digitalWrite(Rele, HIGH); //inicializa com o rele desligado
     pinMode(Botao1, INPUT_PULLUP); //define o pino do Botao como INPUT (Entrada de dados)
     pinMode(Botao2, INPUT_PULLUP); 
@@ -53,6 +51,10 @@ void setup()
     {
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //Configura o relogio pra hora que o sketch foi compilado
     }
+    rtc.disable32K();
+    rtc.clearAlarm(1);
+    rtc.disableAlarm(1);
+    rtc.disableAlarm(2);
 }
 
 void loop()
@@ -68,16 +70,13 @@ void loop()
             lcd.print("OFF");
         }
     lcd.setCursor(0,1);
-    lcd.print(now.day(), DEC);
-    lcd.print("/");
-    lcd.print(now.month(), DEC);
+    char data[10] = "DD/MM";
+    rtc.now().toString(data);
+    lcd.print(data);
+    char hora[10] = "hh:mm:ss";
+    rtc.now().toString(hora);
     lcd.print(" ");
-    lcd.print(now.hour(), DEC);
-    lcd.print(":");
-    lcd.print(now.minute(), DEC);
-    lcd.print(":");
-    lcd.print(now.second(), DEC);
-
+    lcd.print(hora);
 
     if (digitalRead(Botao1) != 1) {
         opc = 1;
@@ -93,8 +92,7 @@ void loop()
             {
                 delay(400);
 
-                estadoRele = !estadoRele; //inverte os estados do Rele/Led
-                estadoLed = !estadoLed;
+                estadoRele = !estadoRele; //inverte os estados do Rele
 
                 lcd.clear();
                 lcd.setCursor(0,0); //muda o apenas estado do rele no display
@@ -106,7 +104,6 @@ void loop()
                     lcd.print("OFF");
                 }
                 digitalWrite(Rele, estadoRele); //muda o estado do Rele/Led
-                digitalWrite(LED_BUILTIN, estadoLed);
             } 
 
             while (estadoRele == 0) //enquanto o rele estiver ligado
@@ -145,8 +142,7 @@ void loop()
             {
                 delay(400);
 
-                estadoRele = !estadoRele; //inverte os estados do Rele/Led
-                estadoLed = !estadoLed;
+                estadoRele = !estadoRele; //inverte os estados do Rele
 
                 lcd.clear();
                 lcd.setCursor(0,0); //muda o apenas estado do rele no display
@@ -154,8 +150,7 @@ void loop()
                 lcd.print(volumeLimite);
                 lcd.print(" L MAX");
 
-                digitalWrite(Rele, estadoRele); //muda o estado do Rele/Led
-                digitalWrite(LED_BUILTIN, estadoLed);
+                digitalWrite(Rele, estadoRele); //muda o estado do Rele
             }
             
             while (volumeTotal < volumeLimite && estadoRele == 0) 
@@ -192,6 +187,59 @@ void loop()
             opc = 0;
         break;
         
+        case 3:
+            
+
+
+        break;
+
+        case 4:
+            volumeLimite = volumeTotal + limite;
+
+            estadoRele = !estadoRele; //inverte os estados do Rele
+
+            lcd.clear();
+            lcd.setCursor(0,0); //muda o apenas estado do rele no display
+            lcd.print("Rele: ");
+            lcd.print(volumeLimite);
+            lcd.print(" L MAX");
+
+            digitalWrite(Rele, estadoRele);
+
+            while (volumeTotal < volumeLimite && estadoRele == 0) 
+            {
+                if (millis() - tempoIni >= periodo) 
+                {
+                    noInterrupts(); //desabilita a Interrupcao para fazer os calculos
+
+                    fluxo = ((periodo / (millis() - tempoIni)) * contador) / calibracao;
+                    volume = fluxo / 60;
+                    volumeTotal += volume;
+                
+                    lcd.setCursor(0,1);
+                    lcd.print("Volume: ");
+                    lcd.print(volumeTotal);
+                    lcd.print(" L");
+                
+                    contador = 0;
+                    tempoIni = millis();
+                    interrupts(); //habilita a Interrupcao para medir os pulsos
+                }
+                if (digitalRead(Botao3) != 1) {
+                    delay(400);
+                    estadoRele = !estadoRele;
+                    digitalWrite(Rele, estadoRele);
+                }
+            }
+            
+            if (estadoRele == 0) {
+                estadoRele = !estadoRele;
+                digitalWrite(Rele, estadoRele);
+            }
+            lcd.clear();
+            opc = 0;
+        break;
+
         default:
         break;
     }
